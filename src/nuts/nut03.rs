623e4cbf1aa160ded3dd0a6aa60eb6c7a069c1a8
thesimplekid <tsk@thesimplekid.com>
2023-07-28 20:02:33 -0400

    chore: update ln

--- src/nuts/nut03.rs
+++ src/nuts/nut03.rs
@@ -3,13 +3,13 @@
 
 use serde::{Deserialize, Serialize};
 
-pub use crate::Invoice;
+pub use crate::Bolt11Invoice;
 
 /// Mint request response [NUT-03]
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct RequestMintResponse {
     /// Bolt11 payment request
-    pub pr: Invoice,
+    pub pr: Bolt11Invoice,
     /// Random hash MUST not be the hash of invoice
     pub hash: String,
 }
