e830cbf0b7be0b528cf25336aeecd817656bab19
thesimplekid <tsk@thesimplekid.com>
2023-09-24 09:19:50 +0100

    `bindings/cashu-js` nut00 and nu01

---
+++ bindings/cashu-js/src/nuts/nut00/blinded_signature.rs
@@ -0,0 +1,33 @@
+use std::ops::Deref;
+
+use cashu::nuts::nut00::BlindedSignature;
+use wasm_bindgen::prelude::*;
+
+use crate::{nuts::nut01::JsPublicKey, nuts::nut02::JsId, types::JsAmount};
+
+#[wasm_bindgen(js_name = BlindedSignature)]
+pub struct JsBlindedSignature {
+    inner: BlindedSignature,
+}
+
+impl Deref for JsBlindedSignature {
+    type Target = BlindedSignature;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+#[wasm_bindgen(js_class = BlindedSignature)]
+impl JsBlindedSignature {
+    #[allow(clippy::new_without_default)]
+    #[wasm_bindgen(constructor)]
+    pub fn new(id: JsId, amount: JsAmount, c: JsPublicKey) -> Self {
+        Self {
+            inner: BlindedSignature {
+                id: id.deref().clone(),
+                amount: amount.deref().clone(),
+                c: c.deref().clone(),
+            },
+        }
+    }
+}
---
+++ bindings/cashu-js/src/types/bolt11_invoice.rs
@@ -0,0 +1,49 @@
+use std::{ops::Deref, str::FromStr};
+
+use cashu::Bolt11Invoice;
+use wasm_bindgen::prelude::*;
+
+use crate::error::{into_err, Result};
+use crate::types::JsAmount;
+
+#[wasm_bindgen(js_name = Bolt11Invoice)]
+pub struct JsBolt11Invoice {
+    inner: Bolt11Invoice,
+}
+
+impl Deref for JsBolt11Invoice {
+    type Target = Bolt11Invoice;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl From<Bolt11Invoice> for JsBolt11Invoice {
+    fn from(inner: Bolt11Invoice) -> JsBolt11Invoice {
+        JsBolt11Invoice { inner }
+    }
+}
+
+#[wasm_bindgen(js_class = Bolt11Invoice)]
+impl JsBolt11Invoice {
+    #[wasm_bindgen(constructor)]
+    pub fn new(invoice: String) -> Result<JsBolt11Invoice> {
+        Ok(JsBolt11Invoice {
+            inner: Bolt11Invoice::from_str(&invoice).map_err(into_err)?,
+        })
+    }
+
+    /// Amount
+    #[wasm_bindgen(getter)]
+    pub fn amount(&self) -> Option<JsAmount> {
+        self.inner
+            .amount_milli_satoshis()
+            .map(|a| JsAmount::from_msat(a))
+    }
+
+    /// Invoice as string
+    #[wasm_bindgen(js_name = asString)]
+    pub fn as_string(&self) -> String {
+        self.inner.to_string()
+    }
+}
