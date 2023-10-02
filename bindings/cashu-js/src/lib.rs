1e8212c7e73c45025fb101445be4a10ef8743653
thesimplekid <tsk@thesimplekid.com>
2023-10-02 21:35:10 +0100

    pack wasm

--- bindings/cashu-js/src/lib.rs
+++ bindings/cashu-js/src/lib.rs
@@ -1,3 +1,12 @@
+use wasm_bindgen::prelude::*;
+
 pub mod error;
 pub mod nuts;
 pub mod types;
+
+pub use types::JsAmount;
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    console_error_panic_hook::set_once();
+}
