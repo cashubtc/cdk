299c343d5394a6670f478f547a959471b99bbfe2
thesimplekid <tsk@thesimplekid.com>
2023-12-19 21:44:47 +0000

    chore: update nix flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     flakebox = {
       url = "github:rustshop/flakebox";
