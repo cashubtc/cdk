b80f7b64b1b0d08dbeb04307d6578b485a554588
thesimplekid <tsk@thesimplekid.com>
2023-12-26 21:15:46 +0000

    refactor: ffi for v1 mint

--- bindings/cashu-ffi/src/types/mod.rs
+++ bindings/cashu-ffi/src/types/mod.rs
@@ -1,8 +1,11 @@
 pub mod amount;
 pub mod bolt11_invoice;
 pub mod keyset_info;
+pub mod mint_quote_info;
 pub mod secret;
 
+pub use amount::Amount;
 pub use bolt11_invoice::Bolt11Invoice;
 pub use keyset_info::KeySetInfo;
+pub use mint_quote_info::MintQuoteInfo;
 pub use secret::Secret;
