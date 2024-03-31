8f5b234b9f5125873cdbf30814dc12a497a187b3
thesimplekid <tsk@thesimplekid.com>
2024-03-31 17:31:38 +0100

    refactor: remove left over blocking

--- crates/cashu-sdk/src/lib.rs
+++ crates/cashu-sdk/src/lib.rs
@@ -1,13 +1,3 @@
-#[cfg(all(target_arch = "wasm32", feature = "blocking"))]
-compile_error!("`blocking` feature can't be enabled for WASM targets");
-
-#[cfg(feature = "blocking")]
-use futures_util::Future;
-#[cfg(feature = "blocking")]
-use once_cell::sync::Lazy;
-#[cfg(feature = "blocking")]
-use tokio::runtime::Runtime;
-
 #[cfg(feature = "wallet")]
 pub mod client;
 
@@ -19,11 +9,3 @@ pub mod wallet;
 
 pub use bip39::Mnemonic;
 pub use cashu::{self, *};
-
-#[cfg(feature = "blocking")]
-static RUNTIME: Lazy<Runtime> = Lazy::new(|| Runtime::new().expect("Can't start Tokio runtime"));
-
-#[cfg(feature = "blocking")]
-pub fn block_on<F: Future>(future: F) -> F::Output {
-    RUNTIME.block_on(future)
-}
