d049026963818bd5f24a341d1fbf008d54e42aeb
thesimplekid <tsk@thesimplekid.com>
2024-04-13 10:56:58 +0100

    refactor: export http client

--- crates/cdk/src/lib.rs
+++ crates/cdk/src/lib.rs
@@ -1,5 +1,7 @@
 extern crate core;
 
+#[cfg(any(feature = "mint", feature = "wallet"))]
+pub use bip39::Mnemonic;
 pub use bitcoin::hashes::sha256::Hash as Sha256;
 pub use bitcoin::secp256k1;
 pub use lightning_invoice::{self, Bolt11Invoice};
@@ -21,6 +23,8 @@ pub mod util;
 pub mod wallet;
 
 pub use self::amount::Amount;
+#[cfg(feature = "wallet")]
+pub use self::client::HttpClient;
 pub use self::util::SECP256K1;
 
 pub type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
