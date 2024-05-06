b6b781c74048bf506563c8d0564731e71f18f305
thesimplekid <tsk@thesimplekid.com>
2024-05-06 21:21:41 +0100

    chore: precommit scripts for bindings

--- misc/scripts/check-crates.sh
+++ misc/scripts/check-crates.sh
@@ -31,6 +31,7 @@ buildargs=(
     "-p cdk-redb"
     "-p cdk-redb --no-default-features --features wallet"
     "-p cdk-redb --no-default-features --features mint"
+    "-p cdk-js --no-default-features --features"
 )
 
 for arg in "${buildargs[@]}"; do
