4a3e74e129cb6cfa6dac19826c54c60404b98c81
thesimplekid <tsk@thesimplekid.com>
2024-05-10 10:41:33 +0100

    refactor(bindings): remove payment method enum

--- bindings/cdk-js/src/nuts/nut00/mod.rs
+++ bindings/cdk-js/src/nuts/nut00/mod.rs
@@ -2,7 +2,6 @@ pub mod blind_signature;
 pub mod blinded_message;
 pub mod currency_unit;
 pub mod mint_proofs;
-pub mod payment_method;
 pub mod premint;
 pub mod proof;
 pub mod token;
@@ -10,7 +9,6 @@ pub mod witness;
 
 pub use blinded_message::JsBlindedMessage;
 pub use currency_unit::JsCurrencyUnit;
-pub use payment_method::JsPaymentMethod;
 pub use premint::{JsPreMint, JsPreMintSecrets};
 pub use proof::JsProof;
 pub use token::JsToken;
