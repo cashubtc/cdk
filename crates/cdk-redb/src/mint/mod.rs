f3f02d4006eb952a73026757da303f29127564eb
thesimplekid <tsk@thesimplekid.com>
2024-06-10 23:26:08 +0100

    feat: mint axum server

--- crates/cdk-redb/src/mint/mod.rs
+++ crates/cdk-redb/src/mint/mod.rs
@@ -309,6 +309,21 @@ impl MintDatabase for MintRedbDatabase {
 
         Ok(current_state)
     }
+    async fn get_mint_quote_by_request(
+        &self,
+        request: &str,
+    ) -> Result<Option<MintQuote>, Self::Err> {
+        let quotes = self.get_mint_quotes().await?;
+
+        let quote = quotes
+            .into_iter()
+            .filter(|q| q.request.eq(request))
+            .collect::<Vec<MintQuote>>()
+            .first()
+            .cloned();
+
+        Ok(quote)
+    }
 
     async fn get_mint_quotes(&self) -> Result<Vec<MintQuote>, Self::Err> {
         let db = self.db.lock().await;
--- crates/cdk-sqlite/src/mint/mod.rs
+++ crates/cdk-sqlite/src/mint/mod.rs
@@ -169,6 +169,31 @@ WHERE id=?;
         Ok(Some(sqlite_row_to_mint_quote(rec)?))
     }
 
+    async fn get_mint_quote_by_request(
+        &self,
+        request: &str,
+    ) -> Result<Option<MintQuote>, Self::Err> {
+        let rec = sqlx::query(
+            r#"
+SELECT *
+FROM mint_quote
+WHERE request=?;
+        "#,
+        )
+        .bind(request)
+        .fetch_one(&self.pool)
+        .await;
+
+        let rec = match rec {
+            Ok(rec) => rec,
+            Err(err) => match err {
+                sqlx::Error::RowNotFound => return Ok(None),
+                _ => return Err(Error::SQLX(err).into()),
+            },
+        };
+
+        Ok(Some(sqlite_row_to_mint_quote(rec)?))
+    }
     async fn update_mint_quote_state(
         &self,
         quote_id: &str,
