39768fea2985f6b35bddbfffd48c3c449ae3dee8
thesimplekid <tsk@thesimplekid.com>
2024-06-11 19:00:08 +0100

    refactor: add ln amount type

--- crates/cdk/src/cdk_lightning/mod.rs
+++ crates/cdk/src/cdk_lightning/mod.rs
@@ -1,5 +1,6 @@
 //! CDK Mint Lightning
 
+use std::fmt;
 use std::pin::Pin;
 
 use async_trait::async_trait;
@@ -9,7 +10,7 @@ use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 use crate::types::InvoiceStatus;
-use crate::{Amount, Sha256};
+use crate::Sha256;
 
 #[derive(Debug, Error)]
 pub enum Error {
@@ -21,6 +22,46 @@ pub enum Error {
     Anyhow(#[from] anyhow::Error),
     #[error(transparent)]
     Parse(#[from] ParseOrSemanticError),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct Amount(u64);
+
+impl Amount {
+    pub const ZERO: Amount = Amount(0);
+
+    /// To Sats
+    pub fn to_sat(&self) -> u64 {
+        self.0 / 1000
+    }
+
+    /// From Sats
+    pub fn from_sat(sat: u64) -> Self {
+        Self(sat * 1000)
+    }
+
+    /// From mSats
+    pub fn from_msat(msat: u64) -> Self {
+        Self(msat)
+    }
+
+    /// To Sats
+    pub fn to_msat(&self) -> u64 {
+        self.0
+    }
+}
+
+impl Default for Amount {
+    fn default() -> Self {
+        Amount::ZERO
+    }
+}
+
+impl fmt::Display for Amount {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} msat", self.0)
+    }
 }
 
 /// Invoice information
