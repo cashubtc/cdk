5b61643168bcffe357c3c178fec53298d44e5f28
thesimplekid <tsk@thesimplekid.com>
2024-07-05 22:50:34 +0100

    chore: typos in precommit

--- misc/scripts/precommit.sh
+++ misc/scripts/precommit.sh
@@ -4,6 +4,7 @@ set -exuo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+typos
 "${DIR}/check-fmt.sh"       # Format the code
 "${DIR}/check-crates.sh"    # Check all crates
 "${DIR}/check-bindings.sh"  # Check all bindings
