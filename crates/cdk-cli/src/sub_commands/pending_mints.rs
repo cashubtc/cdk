7bdc9280d2975d7409d9547152b1dd772e0843fd
thesimplekid <tsk@thesimplekid.com>
2024-07-15 22:36:30 +0100

    feat: support muli unit

--- crates/cdk-cli/src/sub_commands/pending_mints.rs
+++ crates/cdk-cli/src/sub_commands/pending_mints.rs
@@ -1,12 +1,12 @@
 use anyhow::Result;
 use cdk::wallet::MultiMintWallet;
-use cdk::Amount;
 
 pub async fn mint_pending(multi_mint_wallet: &MultiMintWallet) -> Result<()> {
     let amounts = multi_mint_wallet.check_all_mint_quotes(None).await?;
 
-    let amount = amounts.into_values().sum::<Amount>();
+    for (unit, amount) in amounts {
+        println!("Unit: {}, Amount: {}", unit, amount);
+    }
 
-    println!("Amount minted: {amount}");
     Ok(())
 }
