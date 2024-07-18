6705761aec2ae29115bd2f4b78d7a8c1f65806b7
thesimplekid <tsk@thesimplekid.com>
2024-07-19 00:30:59 +0100

    fix: dont update proof state if already spent

--- crates/cdk-redb/src/mint/mod.rs
+++ crates/cdk-redb/src/mint/mod.rs
@@ -590,14 +590,29 @@ impl MintDatabase for MintRedbDatabase {
                 .map_err(Error::from)?;
 
             for y in ys {
-                match table
-                    .insert(y.to_bytes(), state_str.as_str())
-                    .map_err(Error::from)?
+                let current_state;
+
                 {
-                    Some(state) => states.push(Some(
-                        serde_json::from_str(state.value()).map_err(Error::from)?,
-                    )),
-                    None => states.push(None),
+                    match table.get(y.to_bytes()).map_err(Error::from)? {
+                        Some(state) => {
+                            current_state =
+                                Some(serde_json::from_str(state.value()).map_err(Error::from)?)
+                        }
+                        None => current_state = None,
+                    }
+                }
+                states.push(current_state);
+
+                if current_state != Some(State::Spent) {
+                    match table
+                        .insert(y.to_bytes(), state_str.as_str())
+                        .map_err(Error::from)?
+                    {
+                        Some(state) => states.push(Some(
+                            serde_json::from_str(state.value()).map_err(Error::from)?,
+                        )),
+                        None => states.push(None),
+                    }
                 }
             }
         }
