efd1a0b81fc87a3e45e539633334b6fae8c13c82
David Caseria <david@team.sovereign.app>
2024-08-22 11:14:41 -0400

    NWS read timeout

--- crates/cdk/src/wallet/client.rs
+++ crates/cdk/src/wallet/client.rs
@@ -84,7 +84,7 @@ impl HttpClient {
         }
         let client = builder
             .pool_max_idle_per_host(0) // NWS doesn't support keep-alive
-            .connect_timeout(Duration::from_millis(100)) // NWS doesn't end connections properly
+            .read_timeout(Duration::from_millis(100)) // NWS doesn't end connections for invalid urls
             .danger_accept_invalid_certs(accept_invalid_certs) // Allow self-signed certs
             .build()?;
 
