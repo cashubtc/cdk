5cf04034894ce015a9a31c422d6ef5b8bd1dd24b
ok300 <106775972+ok300@users.noreply.github.com>
2024-09-07 23:23:35 +0200

    CLI decode_token: use CBOR diagnostic format instead of pure JSON
    This brings decoded TokenV4s in line with the format specified in NUT-00, especially around how byte array fields are handled.

--- crates/cdk/src/util/mod.rs
+++ crates/cdk/src/util/mod.rs
@@ -3,6 +3,7 @@
 #[cfg(not(target_arch = "wasm32"))]
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use anyhow::Result;
 use bitcoin::secp256k1::{rand, All, Secp256k1};
 #[cfg(target_arch = "wasm32")]
 use instant::SystemTime;
@@ -27,4 +28,15 @@ pub fn unix_time() -> u64 {
         .duration_since(UNIX_EPOCH)
         .unwrap_or_default()
         .as_secs()
+}
+
+/// Serializes a struct to the CBOR diagnostic notation.
+///
+/// See <https://www.rfc-editor.org/rfc/rfc8949.html#name-diagnostic-notation>
+pub fn serialize_to_cbor_diag<T: serde::Serialize>(data: &T) -> Result<String> {
+    let mut cbor_buffer = Vec::new();
+    ciborium::ser::into_writer(data, &mut cbor_buffer)?;
+
+    let diag = cbor_diag::parse_bytes(&cbor_buffer)?;
+    Ok(diag.to_diag_pretty())
 }
