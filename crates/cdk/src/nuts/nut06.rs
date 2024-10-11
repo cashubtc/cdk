b507ce72012ded8050d2e637c195f15d04f08a55
Pavol Rusnak <pavol@rusnak.io>
2024-10-11 14:54:29 +0200

    NUT-06: add urls field

--- crates/cdk/src/nuts/nut06.rs
+++ crates/cdk/src/nuts/nut06.rs
@@ -76,6 +76,9 @@ pub struct MintInfo {
     /// Mint's icon URL
     #[serde(skip_serializing_if = "Option::is_none")]
     pub icon_url: Option<String>,
+    /// Mint's endpoint URLs
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub urls: Option<Vec<String>>,
     /// message of the day that the wallet must display to the user
     #[serde(skip_serializing_if = "Option::is_none")]
     pub motd: Option<String>,
