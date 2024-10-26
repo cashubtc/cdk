c9cfb030e752a113c554bc4a24f9591536ec2a16
thesimplekid <tsk@thesimplekid.com>
2024-10-26 08:49:09 +0100

    chore: clippy

--- crates/cdk/src/mint/types.rs
+++ crates/cdk/src/mint/types.rs
@@ -39,6 +39,7 @@ pub struct MintQuote {
 
 impl MintQuote {
     /// Create new [`MintQuote`]
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         mint_url: MintUrl,
         request: String,
