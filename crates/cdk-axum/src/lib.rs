f554f7582055c2ae83194e060d14efc1b932104a
mubarak23 <mubarakaminu340@gmail.com>
2024-11-05 22:09:52 +0100

    change mint function name to reflect route action

--- crates/cdk-axum/src/lib.rs
+++ crates/cdk-axum/src/lib.rs
@@ -117,7 +117,7 @@ pub struct MintState {
         get_keyset_pubkeys,
         get_keysets,
         get_mint_info,
-        get_mint_bolt11_quote,
+        post_mint_bolt11_quote,
         get_check_mint_bolt11_quote,
         post_mint_bolt11,
         post_melt_bolt11_quote,
@@ -147,7 +147,7 @@ pub async fn create_mint_router(mint: Arc<Mint>, cache_ttl: u64, cache_tti: u64)
         .route("/keysets", get(get_keysets))
         .route("/keys/:keyset_id", get(get_keyset_pubkeys))
         .route("/swap", post(cache_post_swap))
-        .route("/mint/quote/bolt11", post(get_mint_bolt11_quote))
+        .route("/mint/quote/bolt11", post(post_mint_bolt11_quote))
         .route(
             "/mint/quote/bolt11/:quote_id",
             get(get_check_mint_bolt11_quote),
