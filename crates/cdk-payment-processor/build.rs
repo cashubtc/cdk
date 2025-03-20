24a944658159112094d7bd2b7c9ae2aff2453b62
David Caseria <david@team.sovereign.app>
2025-03-20 14:59:14 -0400

    Fix protoc build error

--- crates/cdk-payment-processor/build.rs
+++ crates/cdk-payment-processor/build.rs
@@ -1,5 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=src/proto/payment_processor.proto");
-    tonic_build::compile_protos("src/proto/payment_processor.proto")?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(&["src/proto/payment_processor.proto"], &["src/proto"])?;
     Ok(())
 }
