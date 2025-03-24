17359edce6648aa45fee614571b445c458023e4a
thesimplekid <tsk@thesimplekid.com>
2025-03-24 22:19:45 +0000

    fix: exp for mint-rpc

--- crates/cdk-mint-rpc/build.rs
+++ crates/cdk-mint-rpc/build.rs
@@ -1,5 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=src/proto/cdk-mint-rpc.proto");
-    tonic_build::compile_protos("src/proto/cdk-mint-rpc.proto")?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(&["src/proto/cdk-mint-rpc.proto"], &["src/proto"])?;
     Ok(())
 }
