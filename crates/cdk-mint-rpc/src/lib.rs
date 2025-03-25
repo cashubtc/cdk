594760bff0d2cd8b70014facae51170a4933c206
thesimplekid <tsk@thesimplekid.com>
2025-03-25 17:11:19 +0000

    docs: rpc cli docs

--- crates/cdk-mint-rpc/src/lib.rs
+++ crates/cdk-mint-rpc/src/lib.rs
@@ -1,11 +1,6 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs)]
 #![warn(rustdoc::bare_urls)]
-// Allow missing documentation in generated code
-#![allow(rustdoc::missing_doc_code_examples)]
-// Allow missing docs in the generated protobuf code
-#![allow(missing_docs)]
-
 pub mod proto;
 
 pub mod mint_rpc_cli;
--- crates/cdk-mint-rpc/src/mint_rpc_cli/mod.rs
+++ crates/cdk-mint-rpc/src/mint_rpc_cli/mod.rs
@@ -1,4 +1,7 @@
-/// Module containing command-line interface functionality for the mint RPC client
-/// 
-/// This module provides the CLI interface for interacting with the mint server via RPC
+//! Module containing command-line interface functionality for the mint RPC client
+//!
+//! This module provides the CLI interface for interacting with the mint server via RPC
+//!
+
+/// Subcommands for cli
 pub mod subcommands;
