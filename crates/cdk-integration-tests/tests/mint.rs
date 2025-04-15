f93bd1799b900f5beb12a572db2951287382ad3e
Cesar Rodas <cesar@rodasm.com.py>
2025-04-15 02:11:28 -0300

    Make sure signatory has all the keys in memory
    Drop also foreign constraints on sqlite

--- crates/cdk-integration-tests/tests/mint.rs
+++ crates/cdk-integration-tests/tests/mint.rs
@@ -32,7 +32,9 @@ async fn test_correct_keyset() -> Result<()> {
 
     let mut mint_builder = MintBuilder::new();
     let localstore = Arc::new(database);
-    mint_builder = mint_builder.with_localstore(localstore.clone());
+    mint_builder = mint_builder
+        .with_localstore(localstore.clone())
+        .with_keystore(localstore.clone());
 
     mint_builder = mint_builder
         .add_ln_backend(
