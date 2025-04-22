06a32379777f8c83a7177f7a692a2fec32ace0b5
thesimplekid <tsk@thesimplekid.com>
2025-04-22 10:12:15 +0100

    chore: update stable rust to 1.86.0

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
 
         # Toolchains
         # latest stable
-        stable_toolchain = pkgs.rust-bin.stable."1.83.0".default.override {
+        stable_toolchain = pkgs.rust-bin.stable."1.86.0".default.override {
           targets = [ "wasm32-unknown-unknown" ]; # wasm
           extensions = [ "rustfmt" "clippy" "rust-analyzer" ];
         };
