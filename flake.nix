a18d75e6cfe1dc8a1f98eb280a39f9a66015f959
thesimplekid <tsk@thesimplekid.com>
2025-05-28 10:09:45 -0400

    chore: update flake 25.05 remove nix cache (#769)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "CDK Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
