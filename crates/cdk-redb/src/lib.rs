12adde4dee955829bad9d85215ad181e59fa320c
thesimplekid <tsk@thesimplekid.com>
2025-06-05 12:42:40 +0100

    refactor: remove redb mint database

--- crates/cdk-redb/src/lib.rs
+++ crates/cdk-redb/src/lib.rs
@@ -7,12 +7,8 @@
 pub mod error;
 mod migrations;
 
-#[cfg(feature = "mint")]
-pub mod mint;
 #[cfg(feature = "wallet")]
 pub mod wallet;
 
-#[cfg(feature = "mint")]
-pub use mint::MintRedbDatabase;
 #[cfg(feature = "wallet")]
 pub use wallet::WalletRedbDatabase;
