9ad8b98caa0cdeddbeb84db2c72fdf9fc34f57cd
thesimplekid <tsk@thesimplekid.com>
2025-06-15 07:34:03 +0100

    fix: signatory is not required for wallet

--- crates/cdk-signatory/build.rs
+++ crates/cdk-signatory/build.rs
@@ -2,5 +2,10 @@ fn main() {
     println!("cargo:rerun-if-changed=src/proto/signatory.proto");
 
     #[cfg(feature = "grpc")]
-    tonic_build::compile_protos("proto/signatory.proto").unwrap();
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .type_attribute(".", "#[allow(missing_docs)]")
+        .field_attribute(".", "#[allow(missing_docs)]")
+        .compile_protos(&["src/proto/signatory.proto"], &["src/proto"])
+        .unwrap();
 }
