e016687d2010b3f5b713c267b96d7801824cff12
thesimplekid <tsk@thesimplekid.com>
2025-06-23 10:59:44 +0100

    feat: remove fedimint tonic lnd (#831)
    * feat: remove fedimint tonic lnd

---
+++ crates/cdk-lnd/src/proto/mod.rs
@@ -0,0 +1,9 @@
+#[allow(clippy::all, clippy::pedantic, clippy::restriction, clippy::nursery)]
+pub(crate) mod lnrpc {
+    tonic::include_proto!("lnrpc");
+}
+
+#[allow(clippy::all, clippy::pedantic, clippy::restriction, clippy::nursery)]
+pub(crate) mod routerrpc {
+    tonic::include_proto!("routerrpc");
+}
