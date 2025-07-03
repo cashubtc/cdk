abb43c39b1adfac14c2327c1c5bd253fad412a65
David Caseria <david@team.sovereign.app>
2025-07-03 16:07:14 -0400

    Wallet: Get active mint quotes

--- crates/cdk/src/wallet/mint.rs
+++ crates/cdk/src/wallet/mint.rs
@@ -141,6 +141,16 @@ impl Wallet {
         Ok(total_amount)
     }
 
+    /// Get active mint quotes
+    /// Mint quotes that are not expired and not paid will be returned (including those that are paid but not yet issued).
+    #[instrument(skip(self))]
+    pub async fn get_active_mint_quotes(&self) -> Result<Vec<MintQuote>, Error> {
+        let mut mint_quotes = self.localstore.get_mint_quotes().await?;
+        let unix_time = unix_time();
+        mint_quotes.retain(|quote| quote.state != MintQuoteState::Paid && quote.expiry > unix_time);
+        Ok(mint_quotes)
+    }
+
     /// Mint
     /// # Synopsis
     /// ```rust,no_run
