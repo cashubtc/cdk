4d5fe20cf77b5fdc625d5d8a8fc2ec98d78baade
David Caseria <david@team.sovereign.app>
2025-07-16 12:29:08 -0400

    Add transaction for pending melt

--- crates/cdk/src/wallet/melt/mod.rs
+++ crates/cdk/src/wallet/melt/mod.rs
@@ -1,2 +1,56 @@
+use std::collections::HashMap;
+
+use cdk_common::{
+    util::unix_time,
+    wallet::{MeltQuote, Transaction, TransactionDirection},
+    Error, MeltQuoteBolt11Response, MeltQuoteState, ProofsMethods,
+};
+
+use crate::Wallet;
+
 mod melt_bolt11;
 mod melt_bolt12;
+
+impl Wallet {
+    /// Get all active melt quotes from the wallet
+    pub async fn get_active_melt_quotes(&self) -> Result<Vec<MeltQuote>, Error> {
+        todo!()
+    }
+
+    pub(crate) async fn add_transaction_for_pending_melt(
+        &self,
+        quote: &MeltQuote,
+        response: &MeltQuoteBolt11Response<String>,
+    ) -> Result<(), Error> {
+        if quote.state != response.state {
+            tracing::info!(
+                "Quote melt {} state changed from {} to {}",
+                quote.id,
+                quote.state,
+                response.state
+            );
+            if response.state == MeltQuoteState::Paid {
+                let pending_proofs = self.get_pending_proofs().await?;
+                let proofs_total = pending_proofs.total_amount().unwrap_or_default();
+                let change_total = response.change_amount().unwrap_or_default();
+                self.localstore
+                    .add_transaction(Transaction {
+                        mint_url: self.mint_url.clone(),
+                        direction: TransactionDirection::Outgoing,
+                        amount: response.amount,
+                        fee: proofs_total
+                            .checked_sub(response.amount)
+                            .and_then(|amt| amt.checked_sub(change_total))
+                            .unwrap_or_default(),
+                        unit: quote.unit.clone(),
+                        ys: pending_proofs.ys()?,
+                        timestamp: unix_time(),
+                        memo: None,
+                        metadata: HashMap::new(),
+                    })
+                    .await?;
+            }
+        }
+        Ok(())
+    }
+}
