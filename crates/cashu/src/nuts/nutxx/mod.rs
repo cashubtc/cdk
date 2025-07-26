ead38673e62a184ebfb221fe58e51f50adf95808
Vincent <>
2025-07-26 16:49:06 +0200

    feat: verify cc test

--- crates/cashu/src/nuts/nutxx/mod.rs
+++ crates/cashu/src/nuts/nutxx/mod.rs
@@ -3,18 +3,20 @@
 //! <https://github.com/cashubtc/nuts/blob/main/xx.md>
 
 use bitcoin::hashes::sha256::Hash as Sha256Hash;
+// use cairo_air::utils::{serialize_proof_to_file, ProofFormat};
+use cairo_air::verifier::verify_cairo;
+use cairo_air::{CairoProof, PreProcessedTraceVariant};
 use serde::{Deserialize, Serialize};
+use starknet_types_core::felt::Felt;
+use stwo_cairo_prover::stwo::core::vcs::blake2_merkle::{
+    Blake2sMerkleChannel, Blake2sMerkleHasher,
+};
 use thiserror::Error;
 
 // use super::nut00::Witness;
 use super::nut01::PublicKey;
 use super::{Conditions, Proof};
 use crate::nuts::nut00::BlindedMessage;
-
-use starknet_types_core::felt::Felt;
-// use stwo_cairo_prover::stwo::core::vcs::blake2_merkle::{
-//     Blake2sMerkleChannel, Blake2sMerkleHasher,
-// };
 
 pub mod serde_cc_witness;
 
@@ -55,6 +57,7 @@ impl CCWitness {
 impl Proof {
     /// Verify CC
     pub fn verify_cc(&self) -> Result<(), Error> {
+        // let secret: Nut10Secret = self.secret.clone().try_into()?;
         Err(Error::NotImplemented)
     }
 }
@@ -100,7 +103,59 @@ impl SpendingConditions {}
 // impl TryFrom<Nut10Secret> for SpendingConditions {}
 // impl From<SpendingConditions> for super::nut10::Secret {}
 
+fn verify_cc_test(
+    _secret_data: String, // TODO: verify this also, should have type `SecretData`
+    witness: &CCWitness,
+    with_pedersen: bool,
+) -> Result<(), Error> {
+    // info!("Verifying proof from: {:?}", proof);
+    let cairo_proof = serde_json::from_str::<CairoProof<Blake2sMerkleHasher>>(&witness.proof)
+        .expect("Failed to deserialize Cairo proof");
+    let preprocessed_trace = match with_pedersen {
+        true => PreProcessedTraceVariant::Canonical,
+        false => PreProcessedTraceVariant::CanonicalWithoutPedersen,
+    };
+    let result = verify_cairo::<Blake2sMerkleChannel>(cairo_proof, preprocessed_trace);
+    match result {
+        Ok(_) => Ok(()),
+        Err(_) => Err(Error::NotImplemented), // TODO: find better error
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+    // use crate::nuts::nut00::{Amount, Id, Secret, Witness};
+
+    #[test]
+    fn test_verify() {
+        let proof_json = {
+            let path = PathBuf::from("./example_proof.json");
+            std::fs::read_to_string(path).unwrap()
+        };
+        let witness = CCWitness { proof: proof_json };
+        verify_cc_test("".to_string(), &witness, false).unwrap();
+        assert!(verify_cc_test("".to_string(), &witness, true).is_ok());
+
+        // TODO: make it work with the real verify_cc:
+        // let valid_proof: Proof = Proof {
+        //     amount: Amount::from(100),
+        //     keyset_id: Id::from_str("009a1f293253e41e").unwrap(),
+        //     secret: Secret::generate(),
+        //     c: PublicKey::from_str(
+        //         "02bc9097997d81afb2cc7346b5e4345a9346bd2a506eb7958598a72f0cf85163ea",
+        //     )
+        //     .unwrap(),
+        //     witness: Some(Witness::CCWitness(witness)),
+        //     dleq: None,
+        // };
+        // valid_proof.verify_cc().unwrap();
+        // assert!(valid_proof.verify_cc().is_ok());
+
+        // let invalid_proof: Proof = // TODO: example of an invalid proof
+        // assert!(invalid_proof.verify_cc().is_err());
+    }
     // TODO: write tests
 }
