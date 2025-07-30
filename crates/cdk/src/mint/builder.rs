69140943b8b94510e0ce0e05bf8de98cb6ca17be
asmo <asmogo@protonmail.com>
2025-07-31 00:58:36 +0200

    chore: remove unused Prometheus conditional import in mint builder

--- crates/cdk/src/mint/builder.rs
+++ crates/cdk/src/mint/builder.rs
@@ -11,7 +11,6 @@ use cdk_common::nut05::MeltMethodOptions;
 use cdk_common::payment::Bolt11Settings;
 #[cfg(feature = "auth")]
 use cdk_common::{nut21, nut22};
-#[cfg(feature = "prometheus")]
 use cdk_prometheus::CdkMetrics;
 use cdk_signatory::signatory::Signatory;
 
