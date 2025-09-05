ded165f40558618fdb72b6816be5ec6084efbc7b
C <cesar@rodasm.com.py>
2025-09-05 19:07:04 -0300

    Update the signatory.proto file to match NUT-XXX (#1032)
    * Update the signatory.proto file to match NUT-XXX
    
    Source: https://github.com/cashubtc/nuts/pull/250/files
    
    * Add unit tests as requested in https://github.com/cashubtc/cdk/pull/1032#discussion_r2321436860
    
    * Remove unused types from proto file

--- crates/cdk-sql-common/src/mint/migrations.rs
+++ crates/cdk-sql-common/src/mint/migrations.rs
@@ -29,4 +29,6 @@ pub static MIGRATIONS: &[(&str, &str, &str)] = &[
     ("sqlite", "20250819200000_remove_request_lookup_kind_constraints.sql", include_str!(r#"./migrations/sqlite/20250819200000_remove_request_lookup_kind_constraints.sql"#)),
     ("sqlite", "20250901090000_add_kv_store.sql", include_str!(r#"./migrations/sqlite/20250901090000_add_kv_store.sql"#)),
     ("postgres", "20250901090000_add_kv_store.sql", include_str!(r#"./migrations/postgres/20250901090000_add_kv_store.sql"#)),
+    ("sqlite", "20250903200000_add_signatory_amounts.sql", include_str!(r#"./migrations/sqlite/20250903200000_add_signatory_amounts.sql"#)),
+    ("postgres", "20250903200000_add_signatory_amounts.sql", include_str!(r#"./migrations/postgres/20250903200000_add_signatory_amounts.sql"#)),
 ];
