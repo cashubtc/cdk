ec3a7737f3bce6eee288319c54a35fb7f0dc9a5f
thesimplekid <tsk@thesimplekid.com>
2025-09-21 20:04:27 +0100

    feat: remove redis cache (#1096)
    * feat: remove redis cache
    
    * feat: remove redis cache

--- crates/cdk-axum/src/cache/backend/mod.rs
+++ crates/cdk-axum/src/cache/backend/mod.rs
@@ -1,7 +1,3 @@
 mod memory;
-#[cfg(feature = "redis")]
-mod redis;
 
 pub use self::memory::InMemoryHttpCache;
-#[cfg(feature = "redis")]
-pub use self::redis::{Config as RedisConfig, HttpCacheRedis};
