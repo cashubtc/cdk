ae0a5ab1bc87943585272c3a252ac90abaae8790
tsk <tsk@thesimplekid.com>
2025-10-24 20:23:54 -0400

    feat: update stable rust (#1207)

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
 
         # Toolchains
         # latest stable
-        stable_toolchain = pkgs.rust-bin.stable."1.86.0".default.override {
+        stable_toolchain = pkgs.rust-bin.stable."1.90.0".default.override {
           targets = [ "wasm32-unknown-unknown" ]; # wasm
           extensions = [
             "rustfmt"
@@ -172,6 +172,8 @@
             msrv = pkgs.mkShell (
               {
                 shellHook = "
+                  cargo update
+                  cargo update home --precise 0.5.11
               ${_shellHook}
               ";
                 buildInputs = buildInputs ++ [ msrv_toolchain ];
