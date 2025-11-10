c6186b9dfa2c86f7e7c50cad67cff34c020ac8a9
asmo <asmogo@protonmail.com>
2025-11-10 22:13:12 +0100

    feat: add melt quote custom

--- crates/cdk/src/wallet/melt/custom.rs
+++ crates/cdk/src/wallet/melt/custom.rs
@@ -1,8 +1,8 @@
-use cdk_common::wallet::MeltQuote;
-use tracing::instrument;
-
 use crate::nuts::{MeltOptions, MeltQuoteCustomRequest};
 use crate::{Error, Wallet};
+use cdk_common::wallet::MeltQuote;
+use cdk_common::PaymentMethod;
+use tracing::instrument;
 
 impl Wallet {
     /// Melt Quote for Custom Payment Method
@@ -11,27 +11,18 @@ impl Wallet {
         &self,
         method: &str,
         request: String,
-        options: Option<MeltOptions>,
+        _options: Option<MeltOptions>,
     ) -> Result<MeltQuote, Error> {
         self.refresh_keysets().await?;
 
-        let _quote_request = MeltQuoteCustomRequest {
+        let quote_request = MeltQuoteCustomRequest {
             method: method.to_string(),
             request: request.clone(),
             unit: self.unit.clone(),
             data: serde_json::Value::Null, // Can be extended for method-specific data
         };
+        let quote_res = self.client.post_melt_custom_quote(quote_request).await?;
 
-        // For now, we'll use the bolt11 quote endpoint until custom endpoints are added
-        // This is a temporary workaround - will be replaced in Phase 3
-        // let quote_res = self.client.post_melt_custom_quote(quote_request).await?;
-
-        // TODO: Once custom HTTP client methods are implemented, use them instead
-        // For now, return an error indicating custom methods need HTTP support
-        return Err(Error::UnsupportedPaymentMethod);
-
-        // The following code will be enabled once HTTP client methods exist:
-        /*
         let quote = MeltQuote {
             id: quote_res.quote,
             amount: quote_res.amount,
@@ -47,6 +38,5 @@ impl Wallet {
         self.localstore.add_melt_quote(quote.clone()).await?;
 
         Ok(quote)
-        */
     }
 }
