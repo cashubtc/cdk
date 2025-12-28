f837405038edde48b9325518304cd11160762072
cloudsupper <cloudsupper@proton.me>
2025-12-28 21:22:31 +0100

    create token fromraw bytes

--- crates/cdk-ffi/src/token.rs
+++ crates/cdk-ffi/src/token.rs
@@ -94,6 +94,13 @@ impl Token {
     pub fn encode(&self) -> String {
         self.to_string()
     }
+    
+    /// Decode token from raw bytes
+    #[uniffi::constructor]
+    pub fn from_raw_bytes(bytes: Vec<u8>) -> Result<Token, FfiError> {
+        let token = cdk::nuts::Token::try_from(&bytes)?;
+        Ok(Token { inner: token })
+    }
 
     /// Decode token from string representation
     #[uniffi::constructor]
