c79a634ff5629121679ca68bed8020d4cc521840
lescuer97 <leoescuer@protonmail.com>
2026-01-07 11:49:05 +0100

    change sql migration

--- crates/cdk-sql-common/src/wallet/migrations/sqlite/20251222000000_add_p2pk_table.sql
+++ crates/cdk-sql-common/src/wallet/migrations/sqlite/20251222000000_add_p2pk_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS p2pk_signing_key (
     pubkey BLOB PRIMARY KEY,
     derivation_index INTEGER NOT NULL,
     derivation_path TEXT NOT NULL,
-    created_time INTEGER NOT NULL DEFAULT (strftime('%s','now'))
+    created_time INTEGER NOT NULL
 );
