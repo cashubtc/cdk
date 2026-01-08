ff94dba137d4bb97385b1d0503f11993f0f77c35
thesimplekid <tsk@thesimplekid.com>
2026-01-08 16:29:12 +0000

    chore: test ci panic (do not merge)

--- fuzz/fuzz_targets/fuzz_currency_unit.rs
+++ fuzz/fuzz_targets/fuzz_currency_unit.rs
@@ -9,6 +9,7 @@ use cashu::nuts::CurrencyUnit;
 fuzz_target!(|data: &str| {
     // Fuzz CurrencyUnit string parsing
     let _ = CurrencyUnit::from_str(data);
+    panic!();
 
     // Fuzz CurrencyUnit JSON parsing
     let _: Result<CurrencyUnit, _> = serde_json::from_str(data);
--- fuzz/fuzz_targets/fuzz_token.rs
+++ fuzz/fuzz_targets/fuzz_token.rs
@@ -7,5 +7,7 @@ use libfuzzer_sys::fuzz_target;
 use cashu::Token;
 
 fuzz_target!(|data: &str| {
-    let _ = Token::from_str(data);
+    // let _ = Token::from_str(data);
+    panic!();
+
 });
