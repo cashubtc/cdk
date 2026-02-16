6f5c0b56b3cbd47520bb83061d016d96c3d6a162
tsk <tsk@thesimplekid.com>
2026-02-16 08:27:45 +0000

    feat: bump to 0.15.0-rc.2 (#1634)

--- flake.nix
+++ flake.nix
@@ -567,6 +567,8 @@
                   cargo update
                   cargo update home --precise 0.5.11
                   cargo update typed-index-collections --precise 3.3.0
+                  cargo update simple_asn1 --precise 0.6.3
+                  cargo update time --precise 0.3.44
               ";
                 buildInputs = buildInputs ++ [ msrv_toolchain ];
                 inherit nativeBuildInputs;
