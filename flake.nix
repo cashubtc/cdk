4261f177ab15f629f912f57d169a483309375e4b
thesimplekid <tsk@thesimplekid.com>
2026-02-23 11:16:17 +0000

    chore: bump rust 1.93.1

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
 
         # Toolchains
         # latest stable
-        stable_toolchain = pkgs.rust-bin.stable."1.93.0".default.override {
+        stable_toolchain = pkgs.rust-bin.stable."1.93.1".default.override {
           targets = [ "wasm32-unknown-unknown" ]; # wasm
           extensions = [
             "rustfmt"
