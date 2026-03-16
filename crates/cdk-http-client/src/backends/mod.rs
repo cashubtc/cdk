bf5be9ee95c68e59c310cb523fc8333e9bd7ed81
lescuer97 <leoescuer@protonmail.com>
2026-03-16 12:29:40 +0100

    fix import

--- crates/cdk-http-client/src/backends/mod.rs
+++ crates/cdk-http-client/src/backends/mod.rs
@@ -1,22 +1,32 @@
 //! HTTP request builder backends
 
-#[cfg(feature = "bitreq")]
+#[cfg(all(feature = "bitreq", not(target_arch = "wasm32")))]
 pub mod bitreq_backend;
 
-#[cfg(feature = "reqwest")]
+#[cfg(all(feature = "reqwest", not(target_arch = "wasm32")))]
 pub mod reqwest_backend;
 
-#[cfg(all(feature = "bitreq", feature = "reqwest"))]
+#[cfg(target_arch = "wasm32")]
+pub mod wasm_backend;
+
+#[cfg(all(not(target_arch = "wasm32"), feature = "bitreq", feature = "reqwest"))]
 compile_error!("`bitreq` and `reqwest` features are mutually exclusive for cdk-http-client.");
 
-#[cfg(not(any(feature = "bitreq", feature = "reqwest")))]
+#[cfg(all(
+    not(target_arch = "wasm32"),
+    not(any(feature = "bitreq", feature = "reqwest"))
+))]
 compile_error!("Enable either the `bitreq` or `reqwest` feature for cdk-http-client.");
 
-#[cfg(feature = "bitreq")]
+#[cfg(all(feature = "bitreq", not(target_arch = "wasm32")))]
 pub use bitreq_backend::BitreqRequestBuilder as RequestBuilder;
-#[cfg(feature = "bitreq")]
+#[cfg(all(feature = "bitreq", not(target_arch = "wasm32")))]
 pub use bitreq_backend::{BitreqRequestBuilder, HttpClient, HttpClientBuilder};
-#[cfg(feature = "reqwest")]
+#[cfg(all(feature = "reqwest", not(target_arch = "wasm32")))]
 pub use reqwest_backend::ReqwestRequestBuilder as RequestBuilder;
-#[cfg(feature = "reqwest")]
+#[cfg(all(feature = "reqwest", not(target_arch = "wasm32")))]
 pub use reqwest_backend::{HttpClient, HttpClientBuilder, ReqwestRequestBuilder};
+#[cfg(target_arch = "wasm32")]
+pub use wasm_backend::WasmRequestBuilder as RequestBuilder;
+#[cfg(target_arch = "wasm32")]
+pub use wasm_backend::{HttpClient, HttpClientBuilder, WasmRequestBuilder};
