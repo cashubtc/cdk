bb5b0a54a4ae6970e7cb202bbabce5a10abf894c
tsk <tsk@thesimplekid.com>
2026-07-21 12:41:11 +0000

    feat: update rust to 1.97.1

--- flake.nix
+++ flake.nix
@@ -123,7 +123,7 @@
 
         # Toolchains
         # latest stable
-        stable_toolchain = pkgs.rust-bin.stable."1.96.0".default.override {
+        stable_toolchain = pkgs.rust-bin.stable."1.97.1".default.override {
           targets = [
             "wasm32-unknown-unknown"
             "aarch64-apple-ios"
@@ -169,7 +169,7 @@
         # Stable toolchain with musl target for static builds (Linux only)
         static_toolchain =
           if muslTarget != null then
-            pkgs.rust-bin.stable."1.96.0".default.override
+            pkgs.rust-bin.stable."1.97.1".default.override
               {
                 targets = [ muslTarget ];
               }
@@ -1679,7 +1679,7 @@
                 ndkHome = "${androidSdk}/libexec/android-sdk/ndk/27.0.12077973";
                 toolchainBin = "${ndkHome}/toolchains/llvm/prebuilt/linux-x86_64/bin";
                 androidRustFlags = "-C link-arg=-Wl,-z,max-page-size=16384 -C link-arg=-Wl,-z,common-page-size=16384";
-                buildToolchain = pkgs.rust-bin.stable."1.96.0".default.override {
+                buildToolchain = pkgs.rust-bin.stable."1.97.1".default.override {
                   targets = [
                     "aarch64-linux-android"
                     "armv7-linux-androideabi"
